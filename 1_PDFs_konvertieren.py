b15d22448d757992ac7f238b4833fd45c09ddb81
DaLaZh91 <61336253+DaLaZh91@users.noreply.github.com>
2026-04-21 03:18:03 +0200

    Refactor PDF conversion script with new paths
    Updated paths and comments for clarity.

--- 1_PDFs_konvertieren.py
+++ 1_PDFs_konvertieren.py
@@ -1,42 +1,36 @@
 # =============================================================================
-# Pakete laden
+# Loading packages
 # =============================================================================
 
 import os
 
 # =============================================================================
-# Funktionen einbinden - AKTUALISIEREN WENN DATEINAME NEU 
+# Import functions - UPDATE IF FILE NAME CHANGES
 # =============================================================================
 
-os.chdir(r'W:\Sonder\lva-93300\Masterarbeiten\Marie Punsmann\Python')
-from Funktionen import *
+os.chdir(r'W:\your_folder\Python')
+from functions import *
 
 # =============================================================================
-# Geschützte PDFs als Ungeschützte PDFs speichern
+# Save protected PDFs as unprotected PDFs
 # =============================================================================
 
-# Kündigung
-vielUmspeichern(r'W:\Sonder\lva-93300\Schriftstücke\Daisy-Daten\Daten sortiert\01760 - Kündigung',
-                r'W:\Sonder\lva-93300\Schriftstücke\Daisy-Daten\Daten - ungeschützt\Kündigung')             
-vielUmspeichern(r'W:\Sonder\lva-93300\Schriftstücke\Daisy-Daten\Daten sortiert\11149 - Kündigung',
-                r'W:\Sonder\lva-93300\Schriftstücke\Daisy-Daten\Daten - ungeschützt\Kündigung')
+# Cancellation
+vielUmspeichern(r'W:\your_folder\sorted data\Cancellation',
+                r'W:\your_folder\data - unprotected\Cancellation')             
 
-# Beitragspause
-vielUmspeichern(r'W:\Sonder\lva-93300\Schriftstücke\Daisy-Daten\Daten sortiert\11127 - Beitragspause',
-                r'W:\Sonder\lva-93300\Schriftstücke\Daisy-Daten\Daten - ungeschützt\Beitragspause')
+# Contribution break
+vielUmspeichern(r'W:\your_folder\sorted data\Contribution_break',
+                r'W:\your_folder\data - unprotected\Contribution_break')
 
-# Beitragsfreistellung
-vielUmspeichern(r'W:\Sonder\lva-93300\Schriftstücke\Daisy-Daten\Daten sortiert\11110 - Beitragsfreistellung',
-                r'W:\Sonder\lva-93300\Schriftstücke\Daisy-Daten\Daten - ungeschützt\Beitragsfreistellung')
+# Premium waiver
+vielUmspeichern(r'W:\your_folder\sorted data\Premium_waiver',
+                r'W:\your_folder\data - unprotected\Premium_waiver')
 
-# Erhöhung
-vielUmspeichern(r'W:\Sonder\lva-93300\Schriftstücke\Daisy-Daten\Daten sortiert\11109 - Erhöhung',
-                r'W:\Sonder\lva-93300\Schriftstücke\Daisy-Daten\Daten - ungeschützt\Erhöhung')
+# Increase
+vielUmspeichern(r'W:\your_folder\sorted data\Increase',
+                r'W:\your_folder\data - unprotected\Increase')
 
-# VN-Wechsel
-vielUmspeichern(r'W:\Sonder\lva-93300\Schriftstücke\Daisy-Daten\Daten sortiert\01730 - VN-Wechsel',
-                r'W:\Sonder\lva-93300\Schriftstücke\Daisy-Daten\Daten - ungeschützt\VN-Wechsel')
-vielUmspeichern(r'W:\Sonder\lva-93300\Schriftstücke\Daisy-Daten\Daten sortiert\11135 - VN-Wechsel',
-                r'W:\Sonder\lva-93300\Schriftstücke\Daisy-Daten\Daten - ungeschützt\VN-Wechsel')             
-vielUmspeichern(r'W:\Sonder\lva-93300\Schriftstücke\Daisy-Daten\Daten sortiert\11136 - VN-Wechsel',
-                r'W:\Sonder\lva-93300\Schriftstücke\Daisy-Daten\Daten - ungeschützt\VN-Wechsel')
+# Policyholder change
+vielUmspeichern(r'W:\your_folder\sorted data\Policyholder_change',
+                r'W:\your_folder\data - unprotected\Policyholder_change')
